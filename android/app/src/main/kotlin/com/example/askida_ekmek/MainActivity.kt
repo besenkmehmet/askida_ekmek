1a3361f3c183b3a2a00a3cc530cde07db9d28e39
besenkmehmet <besenkmehmet@gmail.com>
2021-02-06 13:55:49 +0300

    initial

---
+++ android/app/src/main/kotlin/com/example/askida_ekmek/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.askida_ekmek
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'my_app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
